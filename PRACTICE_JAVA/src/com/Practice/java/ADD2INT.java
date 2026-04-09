2f28625e2d254d1cb883ac3126da451b55598a7e
Aman <amannadaf887@gmail.com>
2026-04-09 19:13:27 +0530

    add 2 integers

---
+++ PRACTICE_JAVA/src/com/Practice/java/ADD2INT.java
@@ -0,0 +1,16 @@
+package com.Practice.java;
+
+import java.util.*;
+
+public class ADD2INT {
+    
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Write your code here
+    int n= scanner.nextInt();
+    int m = scanner.nextInt();
+    int sum;
+    sum = n + m;
+    System.out.print(sum);
+  }
+}
